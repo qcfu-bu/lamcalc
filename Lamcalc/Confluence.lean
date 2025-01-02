91b55d5035e929d969ad1ce696ac4cf40a74b1fe
Qiancheng Fu <qcfu@bu.edu>
2025-01-02 15:38:46 -0500

    ok

--- Lamcalc/Confluence.lean
+++ Lamcalc/Confluence.lean
@@ -1,3 +1,4 @@
+import Lean
 import Lamcalc.Semantics
 open ARS
 
@@ -415,12 +416,12 @@ theorem pstep_diamond : Diamond PStep := by
 theorem pstep_strip :
   m ≈> m1 -> m ~>* m2 -> ∃ m', m1 ~>* m' ∧ m2 ≈> m' := by
   intros p r
-  induction r generalizing m1 p <;> clear m2
-  case R =>
+  induction r generalizing m1 p with
+  | R =>
     exists m1; constructor
     . apply Star.R
     . assumption
-  case SE _ s1 ih =>
+  | SE _ s1 ih =>
     rcases ih p with ⟨m2, ⟨r, s2⟩⟩
     rcases pstep_diamond (step_pstep s1) s2 with ⟨m3, ⟨p1, p2⟩⟩
     exists m3; constructor
@@ -429,16 +430,186 @@ theorem pstep_strip :
 
 theorem step_confluent : Confluent Step := by
   intros x y z r
-  induction r generalizing z <;> clear y
-  case R =>
+  induction r generalizing z with
+  | R =>
     intro h; exists z
     constructor
     assumption
     constructor
-  case SE _ s ih =>
+  | SE _ s ih =>
     intro h
     rcases ih h with ⟨z1, ⟨s1 , s2⟩⟩
     rcases pstep_strip (step_pstep s) s1 with ⟨z2, ⟨s3, s4⟩⟩
     exists z2; constructor
     . assumption
     . apply Star.trans s2 (pstep_red s4)
+
+theorem church_rosser : CR Step := by
+  rw[<-Confluent.cr]
+  apply step_confluent
+
+theorem red_var_inv : .var x ~>* y -> y = .var x := by
+  intro r
+  induction r with
+  | R => rfl
+  | SE r st ih =>
+    subst ih; cases st
+
+theorem red_srt_inv : .srt i ~>* x -> x = .srt i := by
+  intro r
+  induction r with
+  | R => rfl
+  | SE r st ih =>
+    subst ih; cases st
+
+theorem red_pi_inv : .pi a b ~>* x ->
+  ∃ a' b', a ~>* a' ∧ b ~>* b' ∧ x = .pi a' b' := by
+  intro r
+  induction r with
+  | R =>
+    exists a, b
+    repeat constructor
+  | SE r st ih =>
+    have ⟨a1, ⟨b1, ⟨ra1, ⟨rb1, e⟩ ⟩⟩⟩ := ih
+    subst e
+    cases st with
+    | @piA _ a2 _ ra2 =>
+      exists a2, b1
+      repeat' apply And.intro
+      . apply Star.SE
+        apply ra1
+        apply ra2
+      . apply rb1
+      . rfl
+    | @piB _ _ b2 rb2 =>
+      exists a1, b2
+      repeat' apply And.intro
+      . apply ra1
+      . apply Star.SE
+        apply rb1
+        apply rb2
+      . rfl
+
+theorem red_lam_inv : .lam a m ~>* x ->
+  ∃ a' m', a ~>* a' ∧ m ~>* m' ∧ x = .lam a' m' := by
+  intro r
+  induction r with
+  | R =>
+    exists a, m
+    repeat constructor
+  | SE r st ih =>
+    have ⟨a1, ⟨m1, ⟨ra1, ⟨rm1, e⟩ ⟩⟩⟩ := ih
+    subst e
+    cases st with
+    | @lamA _ a2 _ ra2 =>
+      exists a2, m1
+      repeat' apply And.intro
+      . apply Star.SE
+        apply ra1
+        apply ra2
+      . apply rm1
+      . rfl
+    | @lamM _ _ m2 rm2 =>
+      exists a1, m2
+      repeat' apply And.intro
+      . apply ra1
+      . apply Star.SE
+        apply rm1
+        apply rm2
+      . rfl
+
+section Tactics
+open Lean Elab Meta Tactic
+
+-- Eliminate an `Exists` proof `m` using `elim`.
+def existsElim (m : Expr) (elim : Expr -> Expr -> MetaM Expr) : MetaM Expr := do
+  let mType <- whnf $ <-inferType m
+  match mType.getAppFnArgs with
+  | (``Exists, #[a, p]) =>
+    withLocalDecl `x BinderInfo.default a fun x =>
+    withLocalDecl `y BinderInfo.default (.app p x) fun y => do
+      let body <- mkLambdaFVars #[x, y] (<-elim x y)
+      mkAppOptM ``Exists.elim #[none, none, none, m, body]
+  | _ => throwError "existsElim {mType}"
+
+-- Eliminate an `And` proof `m` using `elim`.
+def andElim (m : Expr) (elim : Expr -> Expr -> MetaM Expr) : MetaM Expr := do
+  let mType <- whnf $ <-inferType m
+  match mType.and? with
+  | some (a, b) =>
+    withLocalDecl `x BinderInfo.default a fun x =>
+    withLocalDecl `y BinderInfo.default b fun y => do
+      let body <- mkLambdaFVars #[x, y] (<-elim x y)
+      mkAppM ``And.elim #[body, m]
+  | none => throwError f!"andElim {mType}"
+
+-- Given a proposition consisting of `Exists` and `And`, find all `Eq`s among the conjuncts.
+partial def projEqs (m : Expr) (elim : Array Expr -> MetaM Expr) : MetaM Expr := do
+  let mType <- whnf $ <-inferType m
+  match mType.getAppFn.constName? with
+  | some ``Exists =>
+    existsElim m fun x y => do
+      projEqs x fun eqs1 =>
+      projEqs y fun eqs2 =>
+      elim (eqs1 ++ eqs2)
+  | some ``And =>
+    andElim m fun x y =>
+      projEqs x fun eqs1 =>
+      projEqs y fun eqs2 =>
+      elim (eqs1 ++ eqs2)
+  | some ``Eq => elim #[m]
+  | _ => elim #[]
+
+-- Assuming `id : a === b`, get the associated expression of `id` and the inversion lemmas of `a` and `b`.
+def getConv (goal : MVarId) (id : Name) : MetaM (Expr × Expr × Expr) := do
+  goal.withContext do
+    let lctx <- getLCtx
+    match lctx.findFromUserName? id with
+    | some ldecl =>
+      let declExpr := ldecl.toExpr
+      let declType <- inferType declExpr
+      match declType.app2? ``conv_step with
+      | some (a, b) => return (declExpr, a, b)
+      | _ => throwTacticEx `getConv goal
+    | none => throwTacticEx `getConv goal
+
+-- Apply `church_rosser` theorem to refute impossible conversion.
+def applyCR (goal : MVarId) (m l1 l2 : Expr) : MetaM Expr := do
+  let cr <- mkAppM ``church_rosser #[m]
+  existsElim cr fun _ h =>
+  andElim h fun h1 h2 => do
+    let h1 <- mkAppM' l1 #[h1]
+    let h2 <- mkAppM' l2 #[h2]
+    projEqs h1 fun es1 =>
+    projEqs h2 fun es2 => do
+      let e1 <- mkAppM ``Eq.symm #[es1[0]!]
+      let e2 <- mkAppM ``Eq.trans #[e1, es2[0]!]
+      mkAppOptM ``Tm.noConfusion #[<-goal.getType, none, none, e2]
+
+/-
+  Get the associated inversion lemma for `m`. For more complex languages, the
+  list of inversion lemmas need to be extended. -/
+def getInvLemma (m : Expr) : MetaM Expr := do
+  match m.getAppFn.constName! with
+  | ``Tm.var => return .const ``red_var_inv []
+  | ``Tm.srt => return .const ``red_srt_inv []
+  | ``Tm.pi  => return .const ``red_pi_inv  []
+  | ``Tm.lam => return .const ``red_lam_inv []
+  | _ => throwError `getInvLemma
+
+open Lean Elab Tactic in
+/--
+  `false_conv h` refutes impossilbe conversion proof `h`.  -/
+elab "false_conv" h:ident : tactic =>
+  withMainContext do
+    let goal <- getMainGoal
+    let (m, a, b) <- getConv goal h.getId
+    let lemma_a <- getInvLemma a
+    let lemma_b <- getInvLemma b
+    let pf <- applyCR goal m lemma_a lemma_b
+    closeMainGoal `false_conv pf
+
+end Tactics
+
+example (h : Tm.lam a b === Tm.pi a b) : False := by
+  false_conv h
